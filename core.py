76643551b50a909554b30c043a2c52c8ee115475
Mikhail Mishin <36608032+shefferger@users.noreply.github.com>
2020-09-29 13:22:55 +0300

    Logs now more handy

--- core.py
+++ core.py
@@ -1,6 +1,6 @@
 import requests
-from datetime import datetime
 import vk_api
+import logging as log
 from vk_api.utils import get_random_id
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from cfg import TOKEN, GID
@@ -10,15 +10,8 @@ vk = vk_session.get_api()
 poll = VkBotLongPoll(vk_session, GID)
 
 
-def log(info):
-    info = '\n' + str(datetime.now()) + '\t' + info + '\n'
-    print(info)
-    with open('logs.txt', 'a') as logFile:
-        logFile.write(info)
-
-
 def main():
-    log('LinkCut Vk Bot Started')
+    log.log('LinkCut Vk Bot Started')
     for event in poll.listen():
         if event.type == VkBotEventType.MESSAGE_NEW and event.message:
             if event.from_user and (event.message['text'] != '' or event.message['attachments'] != []):
@@ -44,16 +37,16 @@ def main():
                     if response.status_code in (500, 404, 400):
                         msg = 'Ошибка сервера :('
 
-                log('\nUID: ' + str(uid) + '\tMSG: ' + umsg + '\tRequest: ' + msg)
+                log.log('UID: ' + str(uid) + '\tMSG: ' + umsg + '\tRequest: ' + msg)
                 vk.messages.send(
                     random_id=get_random_id(),
                     message=msg,
                     user_id=uid
                 )
             if event.from_chat:
-                log('its chat')
+                log.log('its chat')
             if event.from_group:
-                log('its group')
+                log.log('its group')
 
 
 if __name__ == '__main__':
